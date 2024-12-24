7f9f38c8bbe6928e253ac1b516789e498d74d0ba
project demo <sumaiyamohammed494@gmail.com>
2024-12-24 13:10:57 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ import './App.css';
+import Hero from './components/Hero';
+ import Nav from './components/Nav';
+const App =()=>{
+  return (
+    <div>
+      <Nav/>
+      <Hero/>
+    </div>
+  )
+}
+export default App;
